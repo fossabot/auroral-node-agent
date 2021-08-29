c753e6872f66e49dff282cc63d7e1ce5b0d342c9
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2021-08-29 15:25:47 +0200

    Complete discovery with dummy response; routes and proxy OK

--- src/api/proxy/controller.ts
+++ src/api/proxy/controller.ts
@@ -48,16 +48,16 @@ export const receiveEvent: EventCtrl = async (req, res) => {
       }
   }
 
-type DiscoveryCtrl = expressTypes.Controller<{ id: string }, { body: JsonType }, {}, JsonType, {}>
+type DiscoveryCtrl = expressTypes.Controller<{ id: string }, { sparql: JsonType }, {}, JsonType, {}>
 
 export const discovery: DiscoveryCtrl = async (req, res) => {
     const { id } = req.params
-    const { body } = req.body
+    const { sparql } = req.body
       try {
-      logger.info('Received discovery to ' + id)
-      logger.info(body)
-      const dummyResp: JsonType = { success: true }
-      return responseBuilder(HttpStatusCode.OK, res, null, dummyResp)
+        logger.info('Received discovery to ' + id)
+        logger.info(sparql)
+        const dummyResp: JsonType = { success: true }
+        return responseBuilder(HttpStatusCode.OK, res, null, dummyResp)
       } catch (err) {
           logger.error(err.message)
           return responseBuilder(HttpStatusCode.INTERNAL_SERVER_ERROR, res, err)
