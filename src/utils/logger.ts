fcaa59e1221b0b69b987d8b2845d3d2b432c63c9
Peter Drahovsky <peter.drahovsky@bavenir.eu>
2023-08-09 10:00:00 +0200

    Update: - option to set logger level

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -42,7 +42,7 @@ const options = {
     )
 },
   console: {
-    level: 'debug',
+    level:  process.env.LOGGER_LEVEL! ? process.env.LOGGER_LEVEL : 'debug',
     handleExceptions: true,
     timestamp: true,
     format: combine(
